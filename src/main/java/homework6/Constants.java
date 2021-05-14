fc473d6cb456b07c6547a749034db26750e6ae60
Evgenii Mikhailov <eugene.mikhailow@gmail.com>
2021-05-15 01:24:41 +0300

    GB2 homework 6

---
+++ src/main/java/homework6/Constants.java
@@ -0,0 +1,9 @@
+package homework6;
+
+public class Constants {
+
+    public static final int PORT = 8189;
+    public static final String STOP_WORD = "/end";
+    public static final String HOST = "localhost";
+
+}
